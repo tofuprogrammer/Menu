f505d140a85e3b2ffe082383fbaa6616834e2da6
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-06 10:50:22 +0000

    Started work on project
    Currently trying to work out the error handling for invalid options. For now, I have chosen to end the loop rather than stick the program in an infinite loop.

--- Menu/main.cpp
+++ Menu/main.cpp
@@ -8,7 +8,29 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int chosenOption = 0;
+    do {
+        std::cout << "Please choose an option: \n";
+        std::cout << "1. Option 1\n";
+        std::cout << "2. Option 2\n";
+        std::cout << "3. Option 3\n";
+        std::cout << "4. Exit\n";
+        std::cin >> chosenOption;
+        if (std::cin.fail()){
+            std::cout << "Invalid option!";
+            chosenOption = 0;
+            break;
+        }
+        switch (chosenOption) {
+            case 1:
+                std::cout << "You have selected option 1\n";
+                break;
+            case 2:
+                std::cout << "You have selected option 2\n";
+                break;
+            case 3:
+                std::cout << "You have selected option 3\n";
+                break;
+        }
+    } while (chosenOption != 4);
 }
